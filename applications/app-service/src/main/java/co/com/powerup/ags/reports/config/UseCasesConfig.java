cd44cc4cf3da22a840bea60249e8025a5f57f47e
Andersson García Sotelo <anderssongarciasotelo@gmail.com>
2025-09-18 14:32:44 -0500

    Added project structure

---
+++ applications/app-service/src/main/java/co/com/powerup/ags/reports/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.powerup.ags.reports.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.powerup.ags.reports.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
