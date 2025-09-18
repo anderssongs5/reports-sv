f46e67fdbf7616286009a28cff1d524bf73f4252
Andersson García Sotelo <anderssongarciasotelo@gmail.com>
2025-09-18 14:40:59 -0500

    Added usecases, model, entry points and driven adapters

---
+++ domain/model/src/main/java/co/com/powerup/ags/reports/model/approvedloanreport/gateways/ApprovedLoanReportRepository.java
@@ -0,0 +1,4 @@
+package co.com.powerup.ags.reports.model.approvedloanreport.gateways;
+
+public interface ApprovedLoanReportRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/powerup/ags/reports/usecase/approvedloanreport/ApprovedLoanReportUseCase.java
@@ -0,0 +1,6 @@
+package co.com.powerup.ags.reports.usecase.approvedloanreport;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class ApprovedLoanReportUseCase {
+}
